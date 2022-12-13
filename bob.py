4e9625bc2a11f706ecc09f755259625aea94eeed
Dominik Boehler <dominik.boehler@gmx.net>
2022-12-13 08:33:19 +0100

    solution in class Dec13,22

--- bob.py
+++ bob.py
@@ -1,23 +1,18 @@
 def response(message):
     """ Lackadasical teenager response simulator. """
-    message = message.strip()
-    
+    message = message.strip() # Take away all whitespace from beginning and end of a string
+
     if not message:
         return "Fine. Be that way!"
-    
-    if is_shouting(message) and is_question(message):
+
+    is_question = message.endswith("?")
+    is_shouting = message.isupper()
+
+    if is_shouting and is_question:
         return "Calm down, I know what I'm doing!"
-    
-    if is_shouting(message):
+    elif is_shouting:
         return "Whoa, chill out!"
-    
-    if is_question(message):
+    elif is_question:
         return "Sure."
 
     return "Whatever."
-
-def is_question(message):
-    return message.endswith("?")
-
-def is_shouting(message):
-    return message.isupper()
